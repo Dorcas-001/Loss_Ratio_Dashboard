558b9b142746af4a1f4b4a1a11d6b161452c9f53
Dorcas-001 <ngeyenbonje@gmail.com>
2024-12-13 11:06:04 +0200

    first commit

--- login.py
+++ login.py
@@ -1,30 +1,18 @@
 import bcrypt
-from pymongo import MongoClient, errors
+from pymongo import MongoClient
 import streamlit as st
 
 # MongoDB setup
 mongo_uri = "mongodb://localhost:27017/"
-try:
-    client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
-    db = client.loss_ratio
-    collection = db.dashboard_users
-except errors.ServerSelectionTimeoutError as err:
-    st.error("Could not connect to MongoDB server. Please check your connection settings.")
-    st.stop()
-except Exception as e:
-    st.error(f"An unexpected error occurred: {e}")
-    st.stop()
+client = MongoClient(mongo_uri)
+db = client.loss_ratio
+collection = db.dashboard_users
 
 # Function to authenticate user
 def authenticate(username, password):
-    try:
-        user = collection.find_one({"username": username})
-        if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
-            return True
-    except errors.PyMongoError as e:
-        st.error(f"An error occurred while accessing the database: {e}")
-    except Exception as e:
-        st.error(f"An unexpected error occurred: {e}")
+    user = collection.find_one({"username": username})
+    if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
+        return True
     return False
 
 # Function to render the login page
