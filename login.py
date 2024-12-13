e94563ceafd2b3831a3debe21942efddb6800779
Dorcas-001 <ngeyenbonje@gmail.com>
2024-12-13 11:05:00 +0200

    first commit

--- login.py
+++ login.py
@@ -1,31 +1,20 @@
-import os
 import bcrypt
 from pymongo import MongoClient, errors
 import streamlit as st
 
-# Load MongoDB URI from environment variable
-mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-if not mongo_uri:
-    st.error("MongoDB URI is not set. Please configure the MONGO_URI environment variable.")
-    st.stop()
-
 # MongoDB setup
+mongo_uri = "mongodb://localhost:27017/"
 try:
     client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
     db = client.loss_ratio
     collection = db.dashboard_users
-    client.server_info()  # Trigger a call to test the connection
 except errors.ServerSelectionTimeoutError as err:
     st.error("Could not connect to MongoDB server. Please check your connection settings.")
-    st.stop()
-except errors.PyMongoError as e:
-    st.error(f"An error occurred while accessing the database: {e}")
     st.stop()
 except Exception as e:
     st.error(f"An unexpected error occurred: {e}")
     st.stop()
 
-    
 # Function to authenticate user
 def authenticate(username, password):
     try:
