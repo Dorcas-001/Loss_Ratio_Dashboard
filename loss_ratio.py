de590f82256ea34f79079a5ea48f1ebbfbf414d6
Dorcas-001 <ngeyenbonje@gmail.com>
2025-02-10 11:23:02 +0200

    first commit

--- loss_ratio.py
+++ loss_ratio.py
@@ -34,6 +34,7 @@ month_order = {
     "May": 5, "June": 6, "July": 7, "August": 8, 
     "September": 9, "October": 10, "November": 11, "December": 12
 }
+current_date = datetime.now()
 
 
 # Function to display the dashboard
